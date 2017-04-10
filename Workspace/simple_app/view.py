442d67cf5cef93c043258732e2e0dabcd742236e
sewerynkaliszewski <kaliszewski1991@gmail.com>
2017-04-10 23:15:04 +0200

    First commit with detailed change

--- Workspace/simple_app/view.py
+++ Workspace/simple_app/view.py
@@ -1,7 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Markup
 from flask_bootstrap import Bootstrap
-from flask_appbuilder.charts.views import DirectByChartView
-from flask_appbuilder.models.sqla.interface import SQLAInterface
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -31,19 +29,6 @@ def help():
 @app.route('/load_data')
 def load_data():
     return render_template('load_data.html')
-
-
-class CountryStats(Model):
-    id = Column(Integer, primary_key=True)
-    stat_date = Column(Date, nullable=True)
-    population = Column(Float)
-    unemployed_perc = Column(Float)
-    poor_perc = Column(Float)
-    college = Column(Float)
-
-
-class CountryDirectChartView(DirectByChartView):
-    datamodel = SQLAInterface(CountryStats)
 
 
 if __name__ == '__main__':
