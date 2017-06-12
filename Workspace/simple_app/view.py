6bfec8d33cc9d16aaabb259cef46432d75bc2033
sewerynkaliszewski <kaliszewski1991@gmail.com>
2017-06-12 22:48:55 +0200

    Third commit with some change in table.html

--- Workspace/simple_app/view.py
+++ Workspace/simple_app/view.py
@@ -5,6 +5,8 @@ app = Flask(__name__)
 Bootstrap(app)
 
 
+
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -13,9 +15,7 @@ def index():
 
 @app.route('/table')
 def table():
-    return render_template('table.html',
-    atten = [{'name': 'LSD768921'}, {'name': 'LSD768969'}, {'name': 'LSD768978'}],
-    shifter = [{'name': 'LSP768935'}, {'name': 'LSP768911'}, {'name': 'LSP768789'}])
+    return render_template('table.html')
 
 
 @app.route('/chart')
@@ -27,11 +27,23 @@ def chart():
 def send_data():
     return render_template('send_data.html')
 
-@app.route('/table', methods=['GET', 'POST'])
-def get_element_from_list():
-    module = request.form.get('select_module')
-    return(str(module))
+@app.route('/get_atten', methods=['GET', 'POST'])
+def get_atten():
+    atten_list = ['LSP768935', 'LSP768911', 'LSP768789']
+    return render_template('table.html', atten = atten_list)
 
+@app.route('/get_shifter', methods=['GET', 'POST'])
+def get_shifter():
+    shifter_list = ['LSD768921', 'LSD768969', 'LSD768978']
+    return render_template('table.html', shifter=shifter_list)
+
+@app.route('/process', methods=['GET', 'POST'])
+def process():
+    name = request.formdata
+
+# @app.route('/', methods=['GET', 'POST'])
+# def get_the_frequency():
+#     freq = request.form.
 # @app.route('/send_data', methods=['GET', 'POST'])
 # def upload_data_file():
 #     if request.method == 'POST':
