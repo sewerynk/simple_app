e80d5577c53cc2a613c317ac97390fa767725d9f
sewerynkaliszewski <kaliszewski1991@gmail.com>
2017-06-18 23:04:42 +0200

    fourth commit with some change in table.html

--- Workspace/simple_app/view.py
+++ Workspace/simple_app/view.py
@@ -1,57 +1,46 @@
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
+from wtforms import Form, StringField, IntegerField
 
 app = Flask(__name__)
 Bootstrap(app)
 
-
+class Modules:
+    atten_list = [{'name':'LSP768935', "value": 45}, {'name':'LSP768911', "value": 43}, {'name':'LSP768789',"value": 41}
+        , {'name': 'LSP768453',"value": 37}, {'name':'LSP768754', "value": 26}]
+    shifter_list= [ {'name':'LSD768921'},{'name':'LSD768969'}, {'name':'LSD768978'}
+        , {'name': 'LSD76678'}, {'name':'LSD768789'}]
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html' )
 
 
 @app.route('/table')
 def table():
-    return render_template('table.html')
+    return render_template('table.html', atten = Modules.atten_list, shifter = Modules.shifter_list)
 
 
 @app.route('/chart')
 def chart():
     return render_template('chart.html')
 
-
-@app.route('/send_data')
-def send_data():
-    return render_template('send_data.html')
-
 @app.route('/get_atten', methods=['GET', 'POST'])
 def get_atten():
-    atten_list = ['LSP768935', 'LSP768911', 'LSP768789']
-    return render_template('table.html', atten = atten_list)
+    atten = request.form.get('atten_get')
+    return(str(atten))
 
 @app.route('/get_shifter', methods=['GET', 'POST'])
 def get_shifter():
-    shifter_list = ['LSD768921', 'LSD768969', 'LSD768978']
-    return render_template('table.html', shifter=shifter_list)
+    shifter = request.form.get('shifter_get')
+    return(str(shifter))
 
-@app.route('/process', methods=['GET', 'POST'])
-def process():
-    name = request.formdata
-
-# @app.route('/', methods=['GET', 'POST'])
-# def get_the_frequency():
-#     freq = request.form.
-# @app.route('/send_data', methods=['GET', 'POST'])
-# def upload_data_file():
-#     if request.method == 'POST':
-#         data_file = request.files['File']
-#         data_file.save()
-
-# @app.route('/test')
+@app.route('/get_atten/<string:name>')
+def get_value(name):
+    return None
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
